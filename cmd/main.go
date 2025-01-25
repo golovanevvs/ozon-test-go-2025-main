98f005a4cdcb696c358396b8b2f386c1abcb3f5a
golovanevvs <lony@inbox.ru>
2025-01-25 22:41:44 +0300

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "ozon-test-go-2025-main/internal/task1"
+
+func main() {
+	task1.Task11()
+	// task2.Task21()
+	// task3.Task31()
+	// task4.Task41()
+	// task5.Task51()
+	// task6.Task61()
+	// task7.Task71()
+}
---
+++ internal/task2/task21_test.go
@@ -0,0 +1,53 @@
+package task2
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestTask## - запускает тест функции Run##
+// Тест требует доработки в зависимости от условия задачи
+func TestTask21(t *testing.T) {
+	// Указать диапазон тестов
+	for i := 1; i <= 35; i++ {
+		file, err := os.Open(fmt.Sprintf("tests/%d", i))
+		if err != nil {
+			continue
+		}
+		defer file.Close()
+
+		t.Run(fmt.Sprintf("Test:%d", i), func(t *testing.T) {
+			in := bufio.NewReader(file)
+
+			expected, err := os.ReadFile(fmt.Sprintf("tests/%d.a", i))
+			require.Nil(t, err)
+
+			var buffer bytes.Buffer
+			out := bufio.NewWriter(&buffer)
+
+			// Указать тестируемую функцию задачи # (вариант #)
+			Run21(in, out)
+
+			out.Flush()
+
+			// Код, требующий доработки в зависимости от условия задачи
+			result, err := io.ReadAll(bufio.NewReader(&buffer))
+			require.Nil(t, err)
+
+			require.Equal(t, string(expected), string(result))
+		})
+	}
+}
+
+// BenchmarkTask## - бенчмарк для задачи # (вариант #)
+func BenchmarkTask21(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Task21()
+	}
+}
